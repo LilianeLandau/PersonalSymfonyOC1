3d2da44376de7c431813b3db21fc01e4726c8d93
LilianeLandau <landauliliane@hotmail.fr>
2025-03-25 14:35:42 +0100

    update without Comment entity

--- src/Entity/Book.php
+++ src/Entity/Book.php
@@ -47,16 +47,10 @@ class Book
     #[ORM\ManyToOne(inversedBy: 'books')]
     private ?Editor $editor = null;
 
-    /**
-     * @var Collection<int, Comment>
-     */
-    #[ORM\OneToMany(targetEntity: Comment::class, mappedBy: 'book')]
-    private Collection $comments;
 
     public function __construct()
     {
         $this->authors = new ArrayCollection();
-        $this->comments = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -183,36 +177,6 @@ class Book
     public function setEditor(?Editor $editor): static
     {
         $this->editor = $editor;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Comment>
-     */
-    public function getComments(): Collection
-    {
-        return $this->comments;
-    }
-
-    public function addComment(Comment $comment): static
-    {
-        if (!$this->comments->contains($comment)) {
-            $this->comments->add($comment);
-            $comment->setBook($this);
-        }
-
-        return $this;
-    }
-
-    public function removeComment(Comment $comment): static
-    {
-        if ($this->comments->removeElement($comment)) {
-            // set the owning side to null (unless already changed)
-            if ($comment->getBook() === $this) {
-                $comment->setBook(null);
-            }
-        }
 
         return $this;
     }
